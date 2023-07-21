5a7c9dac1a80de1b0205fda2b7cdda042ddd3a91
Prajna <prajnapprabhu3@gmail.com>
2023-07-21 16:04:33 +0530

    fix: slider setup

--- client/src/pages/Bajaj-App.jsx
+++ client/src/pages/Bajaj-App.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/app/Header";
+import Slider from "../components/app/Slider";
 
 const BajajApp = () => {
   return (
     <div>
       <Header />
+      <Slider />
     </div>
   );
 };
