301e302e20485b8617643958b0ade630d4bfdfe8
Prajna <prajnapprabhu3@gmail.com>
2023-07-21 14:46:30 +0530

    feat: client routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import BajajApp from "./pages/Bajaj-App";
+import Product from "./pages/Product";
 
 const App = () => {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/app" element={<BajajApp />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/Bajaj-App.jsx
@@ -0,0 +1,5 @@
+const BajajApp = () => {
+  return <div>Bajaj-App</div>;
+};
+
+export default BajajApp;
