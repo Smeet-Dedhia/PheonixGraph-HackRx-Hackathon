40899ab3d9f6ef542c0b545a541cbdde946fd029
Garvit-g <garvit.gopalani@learner.manipal.edu>
2023-07-22 01:11:22 +0530

    Dropdown functionality added

--- client/src/components/dashboard/Dropdown.jsx
+++ client/src/components/dashboard/Dropdown.jsx
@@ -1,10 +1,9 @@
-import cover from "../../../assets/products/cover.png";
-import iphone from "../../../assets/products/iphone.png";
-import charger from "../../../assets/products/charger.png";
-import airpods from "../../../assets/products/airpods.png";
-import React from 'react';
-import DropdownMenu from "../Dropdown";
-
+import React, { useState } from 'react';
+import './Dropdown.css';
+import cover from "../../assets/products/cover.png";
+import iphone from "../../assets/products/iphone.png";
+import charger from "../../assets/products/charger.png";
+import airpods from "../../assets/products/airpods.png";
 const best_bundles = [
   {
     label: "Phone Case",
@@ -260,22 +259,17 @@ const video = [
   },
 ];
 
+const DropdownMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
-const Bundles = () => {
-  return (
-    <div className="flex flex-col justify-center bg-white max-w-3xl px-2 py-4 rounded-lg ">
-      <div className="flex gap-12 ml-4">
-        <div>
-          <h2 className="text-md font-medium">Popular Bundles</h2>
-          <p className="text-sm text-gray-500">Summary</p>
-        </div>
-
-      <DropdownMenu />
-
-        {/* <p className="text-sm">Category</p> */}
-      </div>
-
+  const handleItemClick = (item) => {
+    // console.log(`Selected item: ${item}`);
+    if($(item)==='All categories')
+    {
       <div className="flex gap-4 py-4 px-2">
         {best_bundles.map((product, id) => (
           <div
@@ -287,8 +281,84 @@ const Bundles = () => {
           </div>
         ))}
       </div>
+    }
+    if($(item)==='Books')
+    {
+      <div className="flex gap-4 py-4 px-2">
+        {books.map((product, id) => (
+          <div
+            key={id}
+            className="bg-[#E2EBFC] flex flex-col items-center gap-2 justify-center h-28 w-28 rounded-lg"
+          >
+            <img src={product.img} alt="hello" className="h-16" />
+            <p className="text-xs font-semibold">{product.label}</p>
+          </div>
+        ))}
+      </div>
+    }
+    if($(item)==='Music')
+    {
+      <div className="flex gap-4 py-4 px-2">
+        {music.map((product, id) => (
+          <div
+            key={id}
+            className="bg-[#E2EBFC] flex flex-col items-center gap-2 justify-center h-28 w-28 rounded-lg"
+          >
+            <img src={product.img} alt="hello" className="h-16" />
+            <p className="text-xs font-semibold">{product.label}</p>
+          </div>
+        ))}
+      </div>
+    }
+    if($(item)==='DVD')
+    {
+      <div className="flex gap-4 py-4 px-2">
+        {DVD.map((product, id) => (
+          <div
+            key={id}
+            className="bg-[#E2EBFC] flex flex-col items-center gap-2 justify-center h-28 w-28 rounded-lg"
+          >
+            <img src={product.img} alt="hello" className="h-16" />
+            <p className="text-xs font-semibold">{product.label}</p>
+          </div>
+        ))}
+      </div>
+    }
+    if($(item)==='Video')
+    {
+      <div className="flex gap-4 py-4 px-2">
+        {best_bundles.map((product, id) => (
+          <div
+            key={id}
+            className="bg-[#E2EBFC] flex flex-col items-center gap-2 justify-center h-28 w-28 rounded-lg"
+          >
+            <img src={product.img} alt="hello" className="h-16" />
+            <p className="text-xs font-semibold">{product.label}</p>
+          </div>
+        ))}
+      </div>
+    }
+    // You can perform any action here based on the selected item
+  };
+
+  const menuItems = ['All categories','Books', 'Videos', 'Music','DVD'];
+
+  return (
+    <div className="dropdown">
+      <button className="dropdown-button" onClick={toggleDropdown}>
+        {isOpen ? 'Close Dropdown' : 'Open Dropdown'}
+      </button>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          {menuItems.map((item, index) => (
+            <li key={index} onClick={() => handleItemClick(item)}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
 
-export default Bundles;
+export default DropdownMenu;
